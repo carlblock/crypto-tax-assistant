bcb3490f118307ec7382dae39d066dd96d637d40
Carl Blockås <carlblockas@gmail.com>
2024-11-03 22:51:09 +0100

    fix: some TS fixes of csv parser

--- src/lib/analyzer/CSVParser.ts
+++ src/lib/analyzer/CSVParser.ts
@@ -11,13 +11,16 @@ class CSVParser {
       .map((line) => {
         const values = line.split(',').map((v) => v.replace(/"/g, '').trim())
         return headers.reduce<Partial<Transaction>>((obj, header, index) => {
+          const newObj = { ...obj }
           // Type assertions for numeric fields
           if (header === 'amount' || header === 'fee' || header === 'balance') {
-            obj[header as keyof Transaction] = parseFloat(values[index])
+            newObj[header as keyof Transaction] = parseFloat(
+              values[index],
+            ) as any
           } else {
-            obj[header as keyof Transaction] = values[index]
+            newObj[header as keyof Transaction] = values[index] as any
           }
-          return obj
+          return newObj
         }, {}) as Transaction
       })
   }
